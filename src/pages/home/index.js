03e7453197c8e3aee2f368f7fbf7ce963939ce17
wumin <rita@RitadeMacBook-Pro.local>
2023-01-12 14:17:52 +0800

    配置路由

---
+++ src/pages/home/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import classes from './index.module.css'
+export const Home=()=>{
+    return (
+        <div className={classes.wrapper}>
+4353
+        </div>
+    )
+}
---
+++ src/pages/test/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import classes from './index.module.css'
+export const Test=()=>{
+    return (
+        <div className={classes.wrapper}>
+test
+        </div>
+    )
+}
